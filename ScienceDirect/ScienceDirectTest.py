3ef5fa65f935cfe50938525809176d4289195d28
August <96606207+axelotls@users.noreply.github.com>
2023-06-12 16:36:09 -0400

    very very big changes. true.

--- ScienceDirect/ScienceDirectTest.py
+++ ScienceDirect/ScienceDirectTest.py
@@ -1,5 +1,8 @@
 import json
 import requests
+import tkinter as tk
+from tkinter import simpledialog
+from tkinter import messagebox
 
 """
 txtdump = open("ScienceDirect", 'w')
@@ -7,33 +10,63 @@ txtdump.write(str(url))
  
 txtdump.close
 """
-abstracts = open("Abstracts/springerNatureAbstracts.txt", "w")
+abstracts = open("ScienceDirect/Elsevier.txt", "w")
+offset = 0
 
-data = json.loads(url.text)
-print(str(data))
 
-def extractAbstracts(numOfPages):
-    abstracts = open("Abstracts/springerNatureAbstracts.txt", "a")
-    for i in range(numOfPages):
-        if data['records'][i]['abstract'] != "":
-            abstract = data['records'][i]['abstract'] + "\n"  # \n isn't required
-            abstracts.writelines(abstract)
+def main():
+    while True:
+        abstracts = open("ScienceDirect/Elsevier.txt", "a")
+        searchTopic = simpledialog.askstring(title="Abstract Retrieval", prompt="Enter a topic to search for: \n\n" + 
+        "To search for multiple topics,enter the query as such: \n\nlesion AND pancreatic (equal to lesion-pancreatic and lesion pancreatic)\nkidney OR renal\nganglia OR tumor AND NOT malignan\n", parent=root)
+        numOfJournals = simpledialog.askstring(title="Abstract Retrieval", prompt="Enter a number of journals: \n\n\t\t", parent=root)
+        extractAbstracts(numOfJournals, searchTopic)
+        print("\n[Abstract Retrieval: Done.]")
+        if (messagebox.askyesno(title="Abstract Retrieval", message="Finished. \tContinue?", ) == False):
+            break
+    abstracts.close
+    quit()
+
+
+def extractAbstracts(numOfJournals, searchTopic):
+    abstracts = open("ScienceDirect/Elsevier.txt", "a")
+    data = urlRequest(offset, searchTopic, numOfJournals)
+    for i in range(int(numOfJournals)):
+        if data['search-results']['entry'][i]['dc:description'] == None:
+            break
+        else:
+            abstract = data['search-results']['entry'][i]['dc:description'] + "\n"  # \n isn't required
+            abstracts.write(str(abstract))
             print(abstract)
-    abstracts.close    
-
-userInput = ""
-while userInput != "A":
-    searchTopic = input("Enter a topic to search for: ")
-    numOfPages = input("Enter a number of pages to search through (max=100): ")
-
-    url = requests.get("https://api.elsevier.com/content/metadata/article?query=aff(broad%20institute)%20and%20pub-date%20is%202014&view=COMPLETE&httpAccept=application/json&apiKey=dc1c396604f71b360feeecd6f767e1d4")
-    data = json.loads(url.text)
-    extractAbstracts(int(numOfPages))
-    
-    userInput = input("Continue? Press 'A' to quit: ")
     abstracts.close
 
 
+
+
+
+
+
+
+def urlRequest(offset, searchTopic, numOfJournals):
+    url = requests.get("https://api.elsevier.com/content/metadata/article?query="
+    + "keywords(" + searchTopic + ")" + "&view=COMPLETE&" + "&start=" + str(offset) + "&count=25&" + "httpAccept=application/json&apiKey=dc1c396604f71b360feeecd6f767e1d4")
+    return json.loads(url.text)
+
+
+
+
+
+
+
+
+
+root = tk.Tk()
+root.geometry('0x0+1000+400')
+
+root.update_idletasks()
+main()
+#root.withdraw()
+root.mainloop()
 
 
 
