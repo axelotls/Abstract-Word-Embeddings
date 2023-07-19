6a2fd975480017cd630c9d7a635a0eef6c9a9b6a
August <96606207+axelotls@users.noreply.github.com>
2023-07-19 14:01:59 -0400

    Updated Abstract

--- RestAPI/RESTtest.py
+++ RestAPI/RESTtest.py
@@ -8,16 +8,12 @@ def RESTAPIJSON(offset, ceiling, searchTopic):
     url = requests.get("https://api.crossref.org/works?query= " + searchTopic 
     + " &rows=" + str(ceiling) + "&offset=" + str(offset) + "&filter=has-abstract:true&mailto=mosesa3@vcu.edu")
     return json.loads(url.text)
-
-def extractAbstracts():
-    abstracts = open("RestAPI/restAPIabstracts.txt", "a")
-    RESTAPIExtraction(999, "kidney OR (renal AND tissue)")
             
 def RESTAPIExtraction(numOfJournals, searchTopic):
     tempJournals = int(numOfJournals)
-    offset = 1
+    offset = 0
 
-    if (tempJournals < 1001):
+    if (tempJournals <= 1000):
         ceiling = tempJournals
         data = RESTAPIJSON(offset, ceiling, searchTopic)
         RESTAPIparser(ceiling, data)
@@ -32,23 +28,24 @@ def RESTAPIExtraction(numOfJournals, searchTopic):
             tempJournals -= 1000
 
             if (tempJournals > 1000):
-                ceiling = 1000
+                continue
             else:
-                ceiling = tempJournals
+                RESTAPIExtraction(tempJournals, searchTopic)
 
 def RESTAPIparser(ceiling, data):
     count = 0
     for i in range(ceiling):
         try:
-            abstract = data['message']['items'][i]['abstract']
+            abstract = str(data['message']['items'][i]['abstract'])
             abstract = re.sub("<\S+>", "", abstract)
             abstract = re.sub("[a-z]*&\S*;", "", abstract)
             abstract = re.sub("\s{2,}", "", abstract)
             abstracts.write(abstract)
             count += 1
-        except (TypeError, JSONDecodeError) as error:
+        except (TypeError) as error:
             break
-    print(count)
+    if count != 0:
+        print(count)
     abstracts.close
 
 searchTopic = input("Enter a topic: ")
